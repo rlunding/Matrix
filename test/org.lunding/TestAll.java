cb6ea7ab07b1c2f4834c9db8659a031259219e08
Lunding <lunding@me.com>
2015-01-27 00:39:08 +0100

    build.xml (ant) added.

---
+++ test/org.lunding/TestAll.java
@@ -0,0 +1,18 @@
+package org.lunding;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Lunding on 27/01/15.
+ */
+
+@RunWith(Suite.class)
+    @Suite.SuiteClasses(
+            {
+                    TestGUI.class,
+                    TestMatrix.class
+            })
+public class TestAll {
+    //dummy class
+}
--- test/org.lunding/TestGUI.java
+++ test/org.lunding/TestGUI.java
@@ -1,6 +1,7 @@
 package org.lunding;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,5 +10,10 @@ public class TestGUI {
     @Before
     public void setUp() throws Exception {
 
+    }
+
+    @Test
+    public void testNothing(){
+        assertTrue(true);
     }
 }
